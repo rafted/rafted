cf24cfdf201b3546b529aeb681d2845af8a246b9
w <pcranaway@tuta.io>
2022-08-24 23:55:24 +0300

    fix somehow

--- protocol/src/encoding.rs
+++ protocol/src/encoding.rs
@@ -3,16 +3,17 @@ use bytes::Buf;
 const SEGMENT_BITS: u8 = 0x7F;
 const CONTINUE_BIT: u8 = 0x80;
 
-type VarInt = i32;
+pub type VarInt = i32;
 
-pub fn read_varint(buf: &[u8]) -> Result<VarInt, String> {
+pub fn read_varint(mut buf: &[u8]) -> Result<VarInt, String> {
     let mut value: i32 = 0;
     let mut position: i8 = 0;
     let mut current_byte: u8;
 
     loop {
         current_byte = buf.get_u8();
-        value |= (current_byte & SEGMENT_BITS) << position;
+
+        value |= ((current_byte & SEGMENT_BITS) << position) as i32;
 
         if (current_byte & CONTINUE_BIT) == 0 {
             break;
