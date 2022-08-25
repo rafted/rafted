5152cf8585cb29ec93e2e36644b8d25b882db1bf
w <pcranaway@tuta.io>
2022-08-25 13:24:30 +0300

    remove handshake packet code

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -1,26 +1,2 @@
-use std::io::{Read, Write};
-
-use packet::Packet;
-
 mod encoding;
 mod packet;
-
-pub struct HandshakePacket;
-
-impl Read for HandshakePacket {
-    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        todo!()
-    }
-}
-
-impl Write for HandshakePacket {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        todo!()
-    }
-
-    fn flush(&mut self) -> std::io::Result<()> {
-        todo!()
-    }
-}
-
-impl Packet for HandshakePacket {}
